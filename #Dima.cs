1a76bb8e9944934436e80b413772ffc77be0c8ea
ReKKeKpro <114856261+ReKKeKpro@users.noreply.github.com>
2022-10-05 16:44:45 +0500

    Add files via upload

---
+++ #Dima.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bd;
+            int a, b, c;
+            Console.WriteLine("Введите 1 число");
+            bd = Console.ReadLine();
+            a = Convert.ToInt32(bd);
+            Console.WriteLine("Введите 2 число");
+            bd = Console.ReadLine();
+            b = Convert.ToInt32(bd);
+            Console.WriteLine("Введите 3 число");
+            bd = Console.ReadLine();
+            c = Convert.ToInt32(bd);
+            int result = a * b - c;
+            Console.WriteLine("Сумма чисел = " + result);
+           
+            
+
+        }
+    }
+}
